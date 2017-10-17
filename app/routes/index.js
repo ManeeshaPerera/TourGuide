5fdfd67f7f4edc9061b6e16bb710e6bc7450875d
chamara liyanage <xperia.chamara@gmail.com>
2017-10-17 08:14:33 +0530

    notification route modifications

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 const appRoutes = require('./app_routes');
+const notificationRoutes = require('./notification_routes');
 module.exports = function(app, db) {
   appRoutes(app, db);
+  notificationRoutes(app, db);
   // Other route groups could go here, in the future
 };
